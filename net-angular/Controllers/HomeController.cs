9341d085ce8ace2269f0b071cb47924625f3e60a
Pedro Lizarraga <pedlizar@WKMZTD554414.global.publicisgroupe.net>
2023-10-25 15:15:12 -0700

    demo

---
+++ net-angular/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using net_angular.Models;
+
+namespace net_angular.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        
+        var scriptFiles = Directory.GetFiles("wwwroot/client", "*.js")
+            .Select(Path.GetFileName)
+            .ToList();
+
+        return View(scriptFiles);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ net-angular/Controllers/OrdersController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using net_angular.Models;
+
+namespace net_angular.Controllers;
+
+public class OrdersController : Controller
+{
+    private readonly ILogger<OrdersController> _logger;
+
+    public OrdersController(ILogger<OrdersController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        
+        var scriptFiles = Directory.GetFiles("wwwroot/client", "*.js")
+            .Select(Path.GetFileName)
+            .ToList();
+
+        return View(scriptFiles);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
